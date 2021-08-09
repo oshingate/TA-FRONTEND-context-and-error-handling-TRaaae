d01f369a24fcf8337340786238bd229b9ff811dc
oshingate <oshingate@gmail.com>
2021-08-09 17:59:00 +0530

    React:Error Boundary

--- block-BRaadi/code/src/components/App.js
+++ block-BRaadi/code/src/components/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Header from "./Header";
-import Main from "./Main";
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
 
 class App extends React.Component {
   render() {
-    let { isDarkMode } = this.state;
+    let { isDarkMode } = false;
     return (
       <>
         <Header isDarkMode={isDarkMode} />
